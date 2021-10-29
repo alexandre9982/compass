71b3d82445cca2d677c7d85a5e9f267f555aad70
alexandre9982 <93144729+alexandre9982@users.noreply.github.com>
2021-10-29 15:36:06 -0400

    Fichiers initiaux du projet MakeCode

---
+++ main.py
@@ -0,0 +1,11 @@
+def on_forever():
+    degrees = input.compass_heading()
+    if degrees < 45:
+        basic.show_string("N")
+    elif degrees < 135:
+        basic.show_string("E")
+    elif degrees < 225:
+        basic.show_string("S")
+    elif degrees < 315:
+        basic.show_string("W")  
+basic.forever(on_forever)
